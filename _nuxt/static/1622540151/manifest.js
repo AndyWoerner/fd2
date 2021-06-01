6fffb358b5a986052c2cf9c696c0a4f13b00b907
Andy Wörner <andy@frozendonkey.com>
2021-06-01 11:54:19 +0200

    preview

---
+++ _nuxt/static/1622540151/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fimprint","\u002F","\u002Fprivacy","\u002Fprojects\u002F2","\u002Fprojects\u002F1","\u002Fprojects\u002F3","\u002Fprojects\u002F4","\u002Fservices\u002F1","\u002Fprojects\u002F5","\u002Fservices\u002F3","\u002Fservices\u002F2"]})
---
+++ _nuxt/static/1622540151/services/3/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/services/3", {data:[{}],fetch:{},mutations:[]});
