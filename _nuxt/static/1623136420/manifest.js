da53b8d46e5e183eb4d82093beca61f83b96f14b
Andy Wörner <andy@frozendonkey.com>
2021-06-08 09:14:28 +0200

    Responsive Design

---
+++ _nuxt/static/1623136420/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fimprint","\u002Fprivacy","\u002Fservices\u002F1","\u002Fservices\u002F3","\u002Fservices\u002F2","\u002Fprojects\u002F4","\u002Fprojects\u002F1","\u002Fprojects\u002F5","\u002Fprojects\u002F3","\u002Fprojects\u002F2"]})
